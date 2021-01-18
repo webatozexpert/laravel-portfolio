30ea5b9e63c19f75c3a2bed0a03c1c78282ca902
webatozexpert <webatozexpert@gmail.com>
2021-01-18 23:54:53 +0600

    Service Delete
    Service Delete

--- admin/public/js/custom.js
+++ admin/public/js/custom.js
@@ -2,3 +2,36 @@ $(document).ready(function () {
 $('#VisitorDt').DataTable();
 $('.dataTables_length').addClass('bs-select');
 });
+
+
+function getServicesData(){
+
+	axios.get('/getServicesData')
+  .then(function (response) {
+
+  	if(response.status==200){
+
+  	$('#mainDiv').removeClass('d-none');
+  	$('#loaderDiv').addClass('d-none');
+
+    var jsonData=response.data;
+    $.each(jsonData, function(i, item) {
+    $('<tr>').html(
+    	"<td><img class='table-img' src="+jsonData[i].service_img +"></td>" +
+    	"<td>"+ jsonData[i].service_name + "</td>" +
+    	"<td>" + jsonData[i].service_des + "</td>" +
+    	"<td><a href=''><i class='fas fa-edit'></i></a></td>" +
+    	"<td><a  data-toggle='modal' data-target='#deleteModal' ><i class='fas fa-trash-alt'></i></a></td>"
+
+    	).appendTo('#service_table');
+   });
+}
+else{
+	$('#loaderDiv').addClass('d-none');
+  	$('#WrongDiv').removeClass('d-none');
+}
+}).catch(function (error) {
+	$('#loaderDiv').addClass('d-none');
+  	$('#WrongDiv').removeClass('d-none');
+});
+}
--- admin/routes/web.php
+++ admin/routes/web.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'HomeController@HomeIndex');
 Route::get('/visitor', 'VisitorController@VisitorIndex');
 Route::get('/service', 'ServiceController@ServiceIndex');
+Route::get('/getServicesData', 'ServiceController@getServiceData');
+Route::post('/ServiceDelete', 'ServiceController@ServiceDelete');
 /*
 |--------------------------------------------------------------------------
 | Web Routes
