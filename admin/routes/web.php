06d2238b098d4a399240b9c82c42df2cec52acbc
Md Lutfor Rahman <61651795+webatozexpert@users.noreply.github.com>
2021-01-20 12:33:32 +0600

    some update
    some update

--- admin/routes/web.php
+++ admin/routes/web.php
@@ -4,70 +4,55 @@ use Illuminate\Support\Facades\Route;
 
 
 
+
 Route::get('/', 'HomeController@HomeIndex');
+
 Route::get('/visitor', 'VisitorController@VisitorIndex');
-Route::get('/service', 'ServiceController@ServiceIndex');
-Route::get('/getServicesData', 'ServiceController@getServiceData');
-Route::post('/ServiceDelete', 'ServiceController@ServiceDelete');
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-
-
-Route::get('/', 'HomeController@HomeIndex')->middleware('loginCheck');
-
-Route::get('/visitor', 'VisitorController@VisitorIndex')->middleware('loginCheck');
 
 
 
 // Admin Panel Service Management
-Route::get('/service', 'ServiceController@ServiceIndex')->middleware('loginCheck');
-Route::get('/getServicesData', 'ServiceController@getServiceData')->middleware('loginCheck');
-Route::post('/ServiceDelete', 'ServiceController@ServiceDelete')->middleware('loginCheck');
-Route::post('/ServiceDetails', 'ServiceController@getServiceDetails')->middleware('loginCheck');
-Route::post('/ServiceUpdate', 'ServiceController@ServiceUpdate')->middleware('loginCheck');
-Route::post('/ServiceAdd', 'ServiceController@ServiceAdd')->middleware('loginCheck');
+Route::get('/service', 'ServiceController@ServiceIndex');
+Route::get('/getServicesData', 'ServiceController@getServiceData');
+Route::post('/ServiceDelete', 'ServiceController@ServiceDelete');
+Route::post('/ServiceDetails', 'ServiceController@getServiceDetails');
+Route::post('/ServiceUpdate', 'ServiceController@ServiceUpdate');
+Route::post('/ServiceAdd', 'ServiceController@ServiceAdd');
 
 
 // Admin Panel Courses manaement
-Route::get('/courses', 'CoursesController@CoursesIndex')->middleware('loginCheck');
-Route::get('/getCoursesData', 'CoursesController@getCoursesData')->middleware('loginCheck');
-Route::post('/CoursesDelete', 'CoursesController@CoursesDelete')->middleware('loginCheck');
-Route::post('/CoursesDetails', 'CoursesController@getCoursesDetails')->middleware('loginCheck');
-Route::post('/CoursesUpdate', 'CoursesController@CoursesUpdate')->middleware('loginCheck');
-Route::post('/CoursesAdd', 'CoursesController@CoursesAdd')->middleware('loginCheck');
+Route::get('/courses', 'CoursesController@CoursesIndex');
+Route::get('/getCoursesData', 'CoursesController@getCoursesData');
+Route::post('/CoursesDelete', 'CoursesController@CoursesDelete');
+Route::post('/CoursesDetails', 'CoursesController@getCoursesDetails');
+Route::post('/CoursesUpdate', 'CoursesController@CoursesUpdate');
+Route::post('/CoursesAdd', 'CoursesController@CoursesAdd');
 
 
 
 // Admin Panel Projects Management
-Route::get('/Project', 'ProjectController@ProjectIndex')->middleware('loginCheck');
-Route::get('/getProjectData', 'ProjectController@getProjectData')->middleware('loginCheck');
-Route::post('/ProjectDetails', 'ProjectController@getProjectDetails')->middleware('loginCheck');
-Route::post('/ProjectDelete', 'ProjectController@ProjectDelete')->middleware('loginCheck');
-Route::post('/ProjectUpdate', 'ProjectController@ProjectUpdate')->middleware('loginCheck');
-Route::post('/ProjectAdd', 'ProjectController@ProjectAdd')->middleware('loginCheck');
+Route::get('/Project', 'ProjectController@ProjectIndex');
+Route::get('/getProjectData', 'ProjectController@getProjectData');
+Route::post('/ProjectDetails', 'ProjectController@getProjectDetails');
+Route::post('/ProjectDelete', 'ProjectController@ProjectDelete');
+Route::post('/ProjectUpdate', 'ProjectController@ProjectUpdate');
+Route::post('/ProjectAdd', 'ProjectController@ProjectAdd');
 
 
 
 // Admin Panel Projects Management
-Route::get('/Contact', 'ContactController@ContactIndex')->middleware('loginCheck');
-Route::get('/getContactData', 'ContactController@getContactData')->middleware('loginCheck');
-Route::post('/ContactDelete', 'ContactController@ContactDelete')->middleware('loginCheck');
+Route::get('/Contact', 'ContactController@ContactIndex');
+Route::get('/getContactData', 'ContactController@getContactData');
+Route::post('/ContactDelete', 'ContactController@ContactDelete');
 
 
 // Admin Panel Review Management
-Route::get('/Review', 'ReviewController@ReviewIndex')->middleware('loginCheck');
-Route::get('/getReviewData', 'ReviewController@getReviewData')->middleware('loginCheck');
-Route::post('/ReviewDetails', 'ReviewController@getReviewDetails')->middleware('loginCheck');
-Route::post('/ReviewDelete', 'ReviewController@ReviewDelete')->middleware('loginCheck');
-Route::post('/ReviewUpdate', 'ReviewController@ReviewUpdate')->middleware('loginCheck');
-Route::post('/ReviewAdd', 'ReviewController@ReviewAdd')->middleware('loginCheck');
+Route::get('/Review', 'ReviewController@ReviewIndex');
+Route::get('/getReviewData', 'ReviewController@getReviewData');
+Route::post('/ReviewDetails', 'ReviewController@getReviewDetails');
+Route::post('/ReviewDelete', 'ReviewController@ReviewDelete');
+Route::post('/ReviewUpdate', 'ReviewController@ReviewUpdate');
+Route::post('/ReviewAdd', 'ReviewController@ReviewAdd');
 
 
 // Admin Panel Review Management
@@ -78,9 +63,8 @@ Route::get('/Logout', 'LoginController@onLogout');
 
 
 // Admin Photo Gallery
-Route::get('/Photo', 'PhotoController@PhotoIndex')->middleware('loginCheck');
-Route::post('/PhotoUpload', 'PhotoController@PhotoUpload')->middleware('loginCheck');
-Route::get('/PhotoJSON', 'PhotoController@PhotoJSON')->middleware('loginCheck');
-Route::get('/PhotoJSONByID/{id}', 'PhotoController@PhotoJSONByID')->middleware('loginCheck');
-Route::post('/PhotoDelete', 'PhotoController@PhotoDelete')->middleware('loginCheck');
-*/
+Route::get('/Photo', 'PhotoController@PhotoIndex');
+Route::post('/PhotoUpload', 'PhotoController@PhotoUpload');
+Route::get('/PhotoJSON', 'PhotoController@PhotoJSON');
+Route::get('/PhotoJSONByID/{id}', 'PhotoController@PhotoJSONByID');
+Route::post('/PhotoDelete', 'PhotoController@PhotoDelete');
--- site/routesold/web.php
+++ site/routesold/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+
 Route::get('/', 'HomeController@HomeIndex');
 Route::post('/contactSend', 'HomeController@ContactSend');
 
